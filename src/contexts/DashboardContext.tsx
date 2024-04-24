e7406c44d1a5a9d8ccde0aca2e7d8b040e43d7a7
renanvilelati <renanvilelati@gmail.com>
2024-04-24 19:04:35 -0300

    Mockando dados no localstorage enquanto não há API

--- src/contexts/DashboardContext.tsx
+++ src/contexts/DashboardContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, ReactNode, useContext, useState } from 'react';
-import { api } from '../services/api';
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+// import { api } from '../services/api';
 import { iDataCard } from '../types/Cards';
 
 interface iDashboardContext {
@@ -19,30 +19,62 @@ export const DashboardContextProvider = ({
   const [loading, setLoading] = useState(false);
   const [cards, setCards] = useState<iDataCard[]>([]);
 
-
-  const getCards = async () => {
+  const getCards =  () => {
     try {
       setLoading(true);
-      const res = await api.get('cards')
-      setCards(res.data);      
+      // const res = await api.get('cards')
+      // setCards(res.data);
+
+      // Enquanto não tem back
+      const storagedCards = JSON.parse(localStorage.getItem('cards') || '[]');
+      setCards(storagedCards);
     } catch (error) {
-      console.error(error);
+      console.error('Error while fetching cards:', error);
     } finally {
       setLoading(false);
     }
   };
 
-  const createCard = async (data: iDataCard) => {
+  const createCard =  (data: iDataCard) => {
     try {
-      setLoading(true);
-      await api.post('cards', data)
-      getCards()      
+      // setLoading(true);
+      // await api.post('cards', data)
+
+      // Enquanto não tem back
+      const updatedCards = [...cards, data];
+      setCards(updatedCards);
+      localStorage.setItem('cards', JSON.stringify(updatedCards));
+
+      getCards();
     } catch (error) {
-      console.error(error);
+      console.error('Error while creating card:', error);
     } finally {
       setLoading(false);
     }
   };
+
+  useEffect( () => {
+    const InitialData = [
+      {
+        id: 1,
+        type: 'Credit',
+        userName: 'Renan Vilela',
+        password: '1715 2154 1471 2114',
+        validate: '2024-05-30T23:47:55.991Z',
+      },
+      {
+        id: 2,
+        type: 'Debit',
+        userName: 'Renan Vilela',
+        password: '2215 1471 2114 3221',
+        validate: '2024-07-24T23:47:55.991Z',
+      },
+    ]
+
+    localStorage.setItem('cards', JSON.stringify(InitialData));
+
+    
+  }, [])
 
   return (
     <DashboardContext.Provider value={{ cards, loading, getCards, createCard }}>
