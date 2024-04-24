d2d7838c64effccb473a8d1df5526b7b963d5f64
renanvilelati <renanvilelati@gmail.com>
2024-04-24 18:18:23 -0300

    feat: show cards quantity on card session

--- src/components/MyCards/style.ts
+++ src/components/MyCards/style.ts
@@ -5,56 +5,40 @@ export const StyledCards = styled.div`
 
   h3 {
     font-size: 1.25rem;
-    padding: 1.5rem 0;
+    padding: 1.5rem 0 0;
   }
 
   .my-cards-header {
     display: flex;
     align-items: center;
+    justify-content: -between;
+    padding-bottom: 1rem;
+    position: relative;
   }
+
+  .title-wrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .title-wrapper span{
+    font-size: 0.875rem;
+    color: ${({ theme }) => theme.colors.gray};
+  }
+
   button {
     background: transparent;
     color: ${({ theme }) => theme.colors.gray};
     transition: all 0.4s;
     padding: 1rem;
+    padding-bottom: 0.8rem;
+    position: absolute;
+    left: 5rem;
 
     &:hover {
       color: ${({ theme }) => theme.colors.primary};
       transform: translateY(-2px);
-    }
-  }
-
-  form {
-    width: 500px;
-    background-color: #333;
-    padding: 2rem;
-    
-
-    .form-group {
-      display: flex;
-      flex-direction: column;
-      margin-bottom: 1rem;
-    }
-
-    input, select {
-      padding: 0.5rem 2rem;
-      border-radius: 8px;
-      background: transparent;
-      color: #FFF;
-      border: 1px solid ${({theme}) => theme.colors.border};
-      &::placeholder {
-        color: ${({theme}) => theme.colors.gray};
-      }
-    }
-
-    option {
-      background-color: #222;
-    }
-
-    button {
-      width: 100%;
-      background-color: #222;
-      border-radius: 8px;
     }
   }
 
