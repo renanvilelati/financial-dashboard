75f83b6a872ed28fb1f748c3b0a654c0f1e247c9
renanvilelati <renanvilelati@gmail.com>
2024-04-24 15:55:00 -0300

    feat: add mock db to json-server and baseURL to axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.API_URL,
+  // baseURL: import.meta.env.API_URL,
+  baseURL: 'http://localhost:4000/',
 });
