ca9eb76169aade6b04fd28fd16e5f9e200c585ed
renanvilelati <renanvilelati@gmail.com>
2024-04-24 15:55:49 -0300

    feat: add router

--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CardPage from './pages/cards';
 import Dashboard from './pages/dashboard/page';
-import SettingsPage from './pages/Settings';
-import SupportPage from './pages/Support';
+import SettingsPage from './pages/settings';
+import SupportPage from './pages/support';
 import UsersPage from './pages/users';
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/cards" element={<CardPage />} />
-        <Route path="/support" element={<SupportPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/users" element={<UsersPage />} />
+          <Route path="/cards" element={<CardPage />} />
+          <Route path="/support" element={<SupportPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </BrowserRouter>
   );
