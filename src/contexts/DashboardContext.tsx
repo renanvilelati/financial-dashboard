5cecf4b505a421fa7482339ebd89354cd7c7a027
renanvilelati <renanvilelati@gmail.com>
2024-04-25 10:51:27 -0300

    remove unecessary mock data

--- src/contexts/DashboardContext.tsx
+++ src/contexts/DashboardContext.tsx
@@ -68,35 +68,7 @@ export const DashboardContextProvider = ({
         userName: 'Renan Vilela',
         password: '2215 1471 2114 3221',
         validate: '2024-07-24T23:47:55.991Z',
-      },
-      {
-        id: 1,
-        type: 'Credit',
-        userName: 'Renan Vilela',
-        password: '1715 2154 1471 2114',
-        validate: '2024-05-30T23:47:55.991Z',
-      },
-      {
-        id: 2,
-        type: 'Debit',
-        userName: 'Renan Vilela',
-        password: '2215 1471 2114 3221',
-        validate: '2024-07-24T23:47:55.991Z',
-      },
-      {
-        id: 1,
-        type: 'Credit',
-        userName: 'Renan Vilela',
-        password: '1715 2154 1471 2114',
-        validate: '2024-05-30T23:47:55.991Z',
-      },
-      {
-        id: 2,
-        type: 'Debit',
-        userName: 'Renan Vilela',
-        password: '2215 1471 2114 3221',
-        validate: '2024-07-24T23:47:55.991Z',
-      },
+      }
     ]
 
     localStorage.setItem('cards', JSON.stringify(InitialData));
